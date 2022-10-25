273da51bbb560cbd843550d4d4b595ad2d5f36ba
ymmz107 <114783169+ymmz107@users.noreply.github.com>
2022-10-25 13:44:23 -0600

    Add files via upload

--- parcial2/PracticaWebDatos/guardarRegistro.php
+++ parcial2/PracticaWebDatos/guardarRegistro.php
@@ -2,20 +2,24 @@
 
     include 'conexion.php';
 
+    $id = $_POST["id"];
     $nombre = $_POST["nombre"];
-    $sexo = $_POST["sexo"];
     $edad = $_POST["edad"];
+    $sexo = $_POST["sexo"];
     $domicilio = $_POST["domicilio"];
     $fecha_nacimiento = $_POST["fecha_nacimiento"];
 
-    $sql = "INSERT INTO usuarios (nombre, edad, sexo, domicilio, fecha_nacimiento)". 
-    "VALUES ('".$nombre."', ".$edad.", ".$sexo.", '".$domicilio."', '".$fecha_nacimiento."')";
+    $sql = "UPDATE usuarios SET nombre='".$nombre."' , edad= ".$edad.",". 
+    "sexo = '".$sexo."', domicilio = '".$domicilio."', fecha_nacimiento ='".$fecha_nacimiento."'".
+    "WHERE id=".$id;
 
     if($conexion->query($sql) === TRUE){
         echo "Registro guardado con Éxito <a href='consultarDatos.php'>Regresar</a>";
     } else {
         echo "Error: ".$sql."<br>".$conexion->error."<br><br><a href='consultarDatos.php'>Regresar</a>";
+
     }
 
     $conexion->close();
+
 ?>
